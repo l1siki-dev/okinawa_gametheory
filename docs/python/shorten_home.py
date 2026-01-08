547a32db1dff7405343d64c82099a75f8c5e3db4
りしき <lisiki.jp@gmail.com>
2026-01-09 08:12:26 +0900

    Update shorten_home.py

--- docs/python/shorten_home.py
+++ docs/python/shorten_home.py
@@ -4,7 +4,7 @@ def on_nav(nav, config, files):
     # Iterate through top-level navigation items
     for item in nav:
         # Check if the item points to the homepage (usually index.md)
-        if item.title == "" or item.title == "index" or item.title == "Home":
+        if item.title == "" or item.title == "Index" or item.title == "Home":
             # You can change this to ":house:" or "🏠" if you prefer
             item.title = ":material-home:"
             break
